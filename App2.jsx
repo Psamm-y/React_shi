07e32f2cddb3b3138d5f118bd05b72dcf8af8ccc
Psammy <ampadupsamuel1@gmail.com>
2025-01-31 15:34:28 +0000

    Update

--- App2.jsx
+++ App2.jsx
@@ -12,7 +12,7 @@ function App2() {
       <Student name="Ichigo Kurosaki" age={21} isStudent={false} location="Pokuase" />
       <Student name="Bernard" age={30} isStudent={false} location="Amasaman" />
       <Student name="Fenuku Reynolds" age={19} isStudent={true} location="Pokuase" />
-      <UserGreeting isLoggedIn={true} userName="Psammy"/>
+      <UserGreeting isLoggedIn={True} userName="Psammy"/>
     </>
   );
 } 
