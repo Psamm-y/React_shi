693d2e7f3562df81dd86aef37c899e4b13b04926
Psammy <ampadupsamuel1@gmail.com>
2025-02-02 00:00:51 +0000

    Update

--- List.jsx
+++ List.jsx
@@ -1,9 +1,15 @@
 
 function List() {
-  const fruits = ['fig','apple',  'cherry','banana', 'date',  'grape'];
+  const fruits = [{id:1, name: 'fig' },
+    {id:2, name: 'apple', calories: 95 },
+    {id:3,name: 'cherry', calories: 50 },
+    {id:4,name: 'banana' , calories: 105 },
+    {id:5,name: 'date', calories: 80 },
+    {id:6,name: 'grape', calories: 60},];
 
-  
-  const ListItems = fruits.map(fruits => <li>{fruits}</li>);
+  const ListItems = fruits.map(fruit => <li key={fruit.name}>{fruit.name}</li>);
+
+
   return(
     <ol>
       {ListItems}
