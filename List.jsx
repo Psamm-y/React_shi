87abece0b21a9a61d8b7e9bbff4a846b0c9fe386
Psammy <ampadupsamuel1@gmail.com>
2025-02-04 21:27:37 +0000

    Update

--- List.jsx
+++ List.jsx
@@ -1,13 +1,16 @@
 
 function List() {
-  const fruits = [{id:1, name: 'fig' },
+  const fruits = [{id:1, name: 'fig',calories:43 },
     {id:2, name: 'apple', calories: 95 },
     {id:3,name: 'cherry', calories: 50 },
     {id:4,name: 'banana' , calories: 105 },
     {id:5,name: 'date', calories: 80 },
     {id:6,name: 'grape', calories: 60},];
 
-  const ListItems = fruits.map(fruit => <li key={fruit.name}>{fruit.name}</li>);
+
+  //fruits.sort((x, y) => x.name.localeCompare(y.name));
+  fruits.sort((a,b => a.calories - b.calories));
+  const ListItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}: &nbsp;<b>{fruit.calories}</b></li>);
 
 
   return(
