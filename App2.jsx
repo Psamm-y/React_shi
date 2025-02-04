8417cd9bbdcd215eebdf87c8fbe2a15421dcb6f3
Psammy <ampadupsamuel1@gmail.com>
2025-02-04 23:29:49 +0000

    Update

--- App2.jsx
+++ App2.jsx
@@ -4,6 +4,12 @@ import Student from './Student.jsx';
 import UserGreeting from './USerGreeting.jsx';
 import List from './List.jsx'
 function App2() {
+  const fruits = [{ id: 1, name: 'fig', calories: 43 },
+  { id: 2, name: 'apple', calories: 95 },
+  { id: 3, name: 'cherry', calories: 50 },
+  { id: 4, name: 'banana', calories: 105 },
+  { id: 5, name: 'date', calories: 80 },
+  { id: 6, name: 'grape', calories: 60 },];
   return (
     <>
       <Card/>
@@ -13,7 +19,7 @@ function App2() {
       <Student name="Bernard" age={30} isStudent={false} location="Amasaman" />
       <Student name="Fenuku Reynolds" age={19} isStudent={true} location="Pokuase" />
       <UserGreeting isLoggedIn={true} userName="Psammy" />
-      <List/>
+      <List />
     </>
   );
 } 
