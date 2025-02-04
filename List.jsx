c727fa259bfe797557bc4435ec85c583ef73d477
Psammy <ampadupsamuel1@gmail.com>
2025-02-04 23:17:44 +0000

    Update

--- List.jsx
+++ List.jsx
@@ -1,26 +1,26 @@
 
 function List() {
-  const fruits = [{id:1, name: 'fig',calories:43 },
-    {id:2, name: 'apple', calories: 95 },
-    {id:3,name: 'cherry', calories: 50 },
-    {id:4,name: 'banana' , calories: 105 },
-    {id:5,name: 'date', calories: 80 },
-    {id:6,name: 'grape', calories: 60},];
+  const fruits = [{ id: 1, name: 'fig', calories: 43 },
+  { id: 2, name: 'apple', calories: 95 },
+  { id: 3, name: 'cherry', calories: 50 },
+  { id: 4, name: 'banana', calories: 105 },
+  { id: 5, name: 'date', calories: 80 },
+  { id: 6, name: 'grape', calories: 60 },];
 
 
   //fruits.sort((x, y) => x.name.localeCompare(y.name));
   //fruits.sort((a,b) => a.calories - b.calories);
 
-  const lowCalFruits= fruits.filter(fruit => fruit.calories < 100);
+  const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
 
   const ListItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}: &nbsp;<b>{fruit.calories}</b></li>);
-  const ListItems2 =lowCalFruits.map(lowCalFruit => <li key={lowCalFruit.id}>{lowCalFruit.name}: &nbsp;<b>{lowCalFruit.calories}</b></li>);
+  const ListItems2 = lowCalFruits.map(lowCalFruit => <li key={lowCalFruit.id}>{lowCalFruit.name}: &nbsp;<b>{lowCalFruit.calories}</b></li>);
 
-  return(
+  return (
     <ol>
       {ListItems}
     </ol>
-    )
-}
+  )
 
-export default List;
+}
+  export default List;
