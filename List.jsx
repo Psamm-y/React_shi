cf981c46d01fba8d9bc3e09412fdc34c596380fe
Psammy <ampadupsamuel1@gmail.com>
2025-02-04 22:28:51 +0000

    Update

--- List.jsx
+++ List.jsx
@@ -14,6 +14,7 @@ function List() {
   const lowCalFruits= fruits.filter(fruit => fruit.calories < 100);
 
   const ListItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}: &nbsp;<b>{fruit.calories}</b></li>);
+  const ListItems2 =lowCalFruits.map(lowCalFruit => <li key={lowCalFruit.id}>{lowCalFruit.name}: &nbsp;<b>{lowCalFruit.calories}</b></li>);
 
   return(
     <ol>
