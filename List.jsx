61027fb1d8116ef31ee42491adbd90f99f8761ba
Psammy <ampadupsamuel1@gmail.com>
2025-02-05 07:02:02 +0000

    Update

--- List.jsx
+++ List.jsx
@@ -1,12 +1,7 @@
 
-function List() {
-  const fruits = [{ id: 1, name: 'fig', calories: 43 },
-  { id: 2, name: 'apple', calories: 95 },
-  { id: 3, name: 'cherry', calories: 50 },
-  { id: 4, name: 'banana', calories: 105 },
-  { id: 5, name: 'date', calories: 80 },
-  { id: 6, name: 'grape', calories: 60 },];
-
+function List(props) {
+  
+  const itemList = props.items;
 
   //fruits.sort((x, y) => x.name.localeCompare(y.name));
   //fruits.sort((a,b) => a.calories - b.calories);
