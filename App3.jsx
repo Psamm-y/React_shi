0e33f3a34ab2bc1da536da67e99cce7ec75c69a7
Psammy <ampadupsamuel1@gmail.com>
2025-02-08 20:22:26 +0000

    Modify files

--- App3.jsx
+++ App3.jsx
@@ -1,11 +1,13 @@
 import Button from './Button2.jsx'
 import ProfilePicture from './ProfilePicture.jsx';
+import MyComponent from 'MyComponent.jsx';
 
 function App3() {
   return (
     <>
       <Button />
       <ProfilePicture />
+      <MyComponent />
     </>
     
   )
