04f8762063db72ff9930a8be3144df0bf1ee9ac2
Psammy <ampadupsamuel1@gmail.com>
2025-02-09 01:59:21 +0000

    Modify files

--- App3.jsx
+++ App3.jsx
@@ -1,15 +1,16 @@
 import Button from './Button2.jsx'
 import ProfilePicture from './ProfilePicture.jsx';
 import MyComponent from './MyComponent.jsx';
-
+import Counter from './Counter.jsx';
 function App3() {
   return (
     <>
       <Button />
       <ProfilePicture />
       <MyComponent name="Samuel" />
-    </>
+      <Counter/>
     
+      </>
   )
 }
 export default App3;
