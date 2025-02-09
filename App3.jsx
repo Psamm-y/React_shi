0ec886e0805a7288704f4efe9c4603dbfde5f536
Psammy <ampadupsamuel1@gmail.com>
2025-02-09 00:48:41 +0000

    Modify files

--- App3.jsx
+++ App3.jsx
@@ -1,6 +1,6 @@
 import Button from './Button2.jsx'
 import ProfilePicture from './ProfilePicture.jsx';
-import MyComponent from 'MyComponent.jsx';
+import MyComponent from './MyComponent.jsx';
 
 function App3() {
   return (
--- MyComponent.jsx
+++ MyComponent.jsx
@@ -1,8 +1,10 @@
-
+import React, {useState} from 'react';
 function MyComponent() {
   
 
-  return (<>
+  return (
+    <>
+      
   </>)
 }
 export default MyComponent;
