bfccbe2d8497019ec99fc4944f36723deb077e07
Psammy <ampadupsamuel1@gmail.com>
2025-02-09 01:27:54 +0000

    Modify files

--- MyComponent.jsx
+++ MyComponent.jsx
@@ -1,16 +1,31 @@
 import React, { useState} from 'react';
 function MyComponent() {
 
-  const [name, setName] = useState();
-  
+  const [name, setName] = useState("Guest");
+  const [age, setAge] = useState(0);
   const updateName = () => {
-    setName("SpongeBob");
+    setName("Adutwumwaa Theresa");
   }
 
+
+  const incrementAge=() => {
+    setAge(age + 2)
+  }
+
+  const setInitial = () => {
+    setAge(0);
+  }
   return (
     <>
       <p>Name: {name}</p>
-      <button onClick = {updateName}>SetName</button>
+      <button onClick={updateName}>Set Name</button>
+      
+          <p>Age: {age}</p>
+      <button onClick={incrementAge}>Increment Age</button>
+      <button onClick={setInitial}>Set Initial</button>
+      
+          <p>Name: {name}</p>
+      <button onClick = {updateName}>Toggle status</button>
   </>)
 }
 export default MyComponent;
