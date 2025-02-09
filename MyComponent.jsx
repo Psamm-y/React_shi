a1bc2f2164f17972a8e4a028c235f729b2b8ab0f
Psammy <ampadupsamuel1@gmail.com>
2025-02-09 01:09:24 +0000

    Modify files

--- MyComponent.jsx
+++ MyComponent.jsx
@@ -1,10 +1,15 @@
-import React, {useState} from 'react';
+import React, { useState} from 'react';
 function MyComponent() {
+  const [name, setName] = useState();
   
+  const updateName = () => {
+    setName("SpongeBob");
+  }
 
   return (
     <>
-      
+      <p>Name: {name}</p>
+      <button onclick = {updateName}>SetName</button>
   </>)
 }
 export default MyComponent;
