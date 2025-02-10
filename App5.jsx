25d1fa23de4575bfa8665ff4424d1e6bc7206b3d
Psammy <ampadupsamuel1@gmail.com>
2025-02-10 19:00:39 +0000

    Add files

---
+++ App5.jsx
@@ -0,0 +1,9 @@
+import Car from './Car.jsx';
+
+function App5() {
+  
+  return (
+    <Car/>
+  )
+}
+export default App5;
--- Car.jsx
+++ Car.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 
 function Car() {
-  
+  const [car, setCar] = useState({year: 2024, make:"Ford", model:"Mustang"});  
+
+
 
   return (
-
-
+    <>
+      <div>
+        <input type="number" value={car.year} onChange={handleYearChange}/><br />
+        <input type="text" value={car.make} onChange={handleMakeChange}/><br />
+        <input type="text" value={car.model} onChange={handleModelChange}/><br />
+    </div>
+    </>
   )
 }
 export default Car;
