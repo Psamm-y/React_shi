0d94869d661f7eb6a952bbe9516f0600879872a5
Psammy <ampadupsamuel1@gmail.com>
2025-02-12 22:07:18 +0000

    Add UpdateArray

---
+++ UpdateArrays.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function UpdateArrays() {
+  const [foods, setFoods] = useState(["Jollof","Fried Rice","Bread"]);
+
+  
+  function handleAddFood() {
+    const newFood = document.getElementById("foodInput").value;
+  }
+  return (
+    <div>
+      <h2>List of Foods</h2>
+      <ul>
+        {foods.map((food, index) =>
+          <li key={index} >
+            {food}
+          </li>)}
+      </ul>
+      <input type="text" id="foodInput" placeholder="Please Enter food name" />
+      <button onClick={handleAddFood}>Add food</button>
+    </div>
+  )
+}
+
+export default UpdateArrays;
