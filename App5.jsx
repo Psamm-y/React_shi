a73a0a04ce483e277d8d587d6ad1610bf13c8dc1
Psammy <ampadupsamuel1@gmail.com>
2025-02-12 21:57:27 +0000

    Update

--- App5.jsx
+++ App5.jsx
@@ -1,9 +1,13 @@
 import Car from './Car.jsx';
+import UpdateArrays from './UpdateArrays.jsx';  
 
 function App5() {
   
   return (
-    <Car/>
+    <>
+    <Car />
+      <UpdateArrays />
+      </>
   )
 }
 export default App5;
