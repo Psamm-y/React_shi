cd6fd5e1beb170172472b16a3c545a92cea76a4f
Psammy <ampadupsamuel1@gmail.com>
2025-02-14 22:48:47 +0000

    Update

--- Todo.jsx
+++ Todo.jsx
@@ -2,9 +2,27 @@ import React,{useState} from "react";
 
 function Todo() {
   const [tasks, setTasks] = useState([]);
-  
+  const [newTasks, setNewTasks] = useState([]);
+
+  function handleInputChange(event) {
+    
+  }
+  function addTask() {
+    
+  }
+  function deleteTask(index) {
+    
+  }
+  function moveTaskUp(index) {
+    
+  }
+  function moveTaskDown() {
+    
+  }
   return (
     <>
+      <div className="to-do-list"></div>
+      <h1></h1>
     </>
 )
 }
