8a2bc9de14efc4a5d597fc46ba6519e2997c3d33
Psammy <ampadupsamuel1@gmail.com>
2025-02-19 22:21:17 +0000

    Update

--- main.jsx
+++ main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App7 from './App7.jsx'
+import App8 from './App8.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App7 />
+    <App8 />
   </StrictMode>
 )
