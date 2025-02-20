a0a62fbe19a9a31958f5937a3176d0ba32f7aa5f
Psammy <ampadupsamuel1@gmail.com>
2025-02-20 00:04:56 +0000

    Update

--- DigitalClock.jsx
+++ DigitalClock.jsx
@@ -9,7 +9,12 @@ function DigitalClock() {
     const intervalId = setInterval(() => {
       setTime(new Date(), 1000)
     })
-  }, []);
+     return () =>
+  {
+    clearInterval(intervalId)
+  }
+  },[]
+  );
   return (
     <>
       <div className="clock">
