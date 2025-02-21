1b8ad5fa7e85cfb0e532fd535006d93654d9bf50
Psammy <ampadupsamuel1@gmail.com>
2025-02-21 23:54:07 +0000

    Update

--- ComponentA.jsx
+++ ComponentA.jsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { useState,createContext } from "react";
 import ComponentB from "./ComponentB";
+
+
 function ComponentA(){
 
   const [user, SetUser] = useState("Psammy");
   return(
     <div className="box">
       <h1>Component A</h1>
+      <h2>{`Hello ${user}`}</h2>
       <ComponentB/>
     </div>
   )
