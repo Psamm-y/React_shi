04f75c27d947e2c7e9e7f1e40cc2dbe1e5e74db3
Psammy <ampadupsamuel1@gmail.com>
2025-02-25 19:06:46 +0000

    Update

--- ComponentC.jsx
+++ ComponentC.jsx
@@ -1,13 +1,13 @@
 import ComponentD from "./ComponentD";
 import { UserContext } from "./ComponentA";
 import { useContext } from "react";
-function ComponentC(){
+function ComponentC() {
   const username = useContext(UserContext);
-  return(
+  return (
     <div className="box">
-    <h1>Component C</h1>
+      <h1>Component C</h1>
       <h2>Hi Again {username}</h2>
-    <ComponentD/>
+      <ComponentD />
     </div>
   )
 
