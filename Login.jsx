189ca3a4b9449e0dc940604387b7f5846e8f583e
Psammy <ampadupsamuel1@gmail.com>
2025-02-27 02:31:28 +0000

    Update

--- Login.jsx
+++ Login.jsx
@@ -1,28 +1,28 @@
-
 function Login() {
-  
-
   return (
     <>
       <div className="login">
         <form>
           <p>Sign Up</p> <br />
           <label htmlFor="email">Email:</label> <br />
-          <input id="email" type="text" placeholder="ampadupsamuel1@gmail.com"/> <br />
+          <input
+            id="email"
+            type="text"
+            placeholder="ampadupsamuel1@gmail.com"
+          />{' '}
+          <br />
           <label htmlFor="name">Name:</label> <br />
-          <input id="name" type="text" placeholder="Fenuku Reynolds"/> <br />
+          <input id="name" type="text" placeholder="Fenuku Reynolds" /> <br />
           <label htmlFor="email">New Password:</label> <br />
           <input id="password" type="text" placeholder="•••••••••••" /> <br />
-                    <label htmlFor="email">Retype Password:</label> <br />
+          <label htmlFor="email">Retype Password:</label> <br />
           <input id="password" type="text" placeholder="•••••••••••" /> <br />
-          
-          
           <div className="terms-and-conditions">
-            <input type="checkbox" id="agree" />
-            </div>
+            <input type="checkbox" id="agree" />p
+          </div>
         </form>
       </div>
     </>
-  )
+  );
 }
 export default Login;
