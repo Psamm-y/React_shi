71d6433b87162982d4269fefbcbf61776a28d986
Psammy <ampadupsamuel1@gmail.com>
2025-03-01 22:31:15 +0000

    Update

--- Login.jsx
+++ Login.jsx
@@ -11,7 +11,7 @@ function Login() {
     <>
       <div className="login">
         <form>
-          <p>Sign Up</p> <br />
+          <p>Login</p> <br />
           <label htmlFor="email">Email:</label> <br />
           <input
             id="email"
@@ -20,20 +20,11 @@ function Login() {
             required
           />
           <br />
-          <br />
           <label htmlFor="email">New Password:</label> <br />
           <input id="password" type="text" placeholder="•••••••••••" /> <br />
-          <label htmlFor="email">Retype Password:</label> <br />
-          <input id="password" type="text" placeholder="•••••••••••" required />
           <br />
-          <div className="terms-and-conditions">
-            <input type="checkbox" id="agree" />
-            <p>
-              &nbsp;I agree to the <a href="#">terms and conditions</a>
-            </p>
-          </div>
           <p>
-            Have an account already? <a href="#">Login</a>
+            New to QuickHomes? <a href="#">Sign up</a>
           </p>
         </form>
       </div>
